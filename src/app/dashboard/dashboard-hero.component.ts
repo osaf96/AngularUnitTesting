f496f87f35199332f5eed46d9a6604d0748eef3b
OC <asim.aziz@lhr.oneclout.com>
2022-03-28 10:19:07 +0500

    initial commit

---
+++ src/app/dashboard/dashboard-hero.component.ts
@@ -0,0 +1,18 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+import { Hero } from '../model/hero';
+
+@Component({
+  selector: 'dashboard-hero',
+  template: `
+    <button type="button" (click)="click()" class="hero">
+      {{hero.name | uppercase}}
+    </button>
+  `,
+  styleUrls: [ './dashboard-hero.component.css' ]
+})
+export class DashboardHeroComponent {
+  @Input() hero!: Hero;
+  @Output() selected = new EventEmitter<Hero>();
+  click() { this.selected.emit(this.hero); }
+}
